93c6b9cd4e54e651220c21e2dad16b5413290a20
ankit-antony-devops <x00193216@mytudublin.ie>
2023-08-17 22:43:22 +0100

    new values added

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
     e.preventDefault();
    
     try{
-      const res = await axios.post('http://localhost:5500/api/item', {item: e.target.item.value, assignee:e.target.assignee.value})
+      const res = await axios.post('REACT_APP_BACKEND_URL/api/item', {item: e.target.item.value, assignee:e.target.assignee.value})
       setListItems(prev => [...prev, res.data]);
       setItemText('');
       setItemAssignee('');
@@ -25,7 +25,7 @@ function App() {
   useEffect(()=>{
     const getItemsList = async () => {
       try{
-        const res = await axios.get('http://localhost:5500/api/items')
+        const res = await axios.get('REACT_APP_BACKEND_URL/api/items')
         setListItems(res.data);
         console.log('render')
       }catch(err){
@@ -38,7 +38,7 @@ function App() {
   // Delete item when click on delete
   const deleteItem = async (id) => {
     try{
-      const res = await axios.delete(`http://localhost:5500/api/item/${id}`)
+      const res = await axios.delete(`REACT_APP_BACKEND_URL/api/item/${id}`)
       const newListItems = listItems.filter(item=> item._id !== id);
       setListItems(newListItems);
     }catch(err){
